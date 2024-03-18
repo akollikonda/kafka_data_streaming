065bc68e6d721d2cd17b14708927a8f897e628a0
Abhilash K <akollikonda-ext@visionbank.com.sa>
2024-03-17 21:49:06 -0400

    Formated API data

--- dags/kafka_streaming.py
+++ dags/kafka_streaming.py
@@ -1,6 +1,6 @@
 from datetime import datetime
-# from airflow import DAG 
-# from airflow.operators.python import PythonOperator
+from airflow import DAG 
+from airflow.operators.python import PythonOperator
 
 
 def get_data():
@@ -14,21 +14,38 @@ def get_data():
     else:
         raise Exception(res.get('status'))
 
+def format_data(res):
+    data = {}
 
+    location = res['location']
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
 
-# default_args = {
-#     'owner':'airflow',
-#     'start_date':datetime(2024,3,17,21,00)
-# }
+    return data
 
-# with DAG('user_automation',
-#     default_args=default_args,
-#     schedule_interval='@daily',
-#     catchup=False
-#     ) as dag:
-#     streaming_task = PythonOperator(
-#         task_id = 'stream_data_from_api',
-#         python_callable=stream_data
-#     )
+default_args = {
+    'owner':'airflow',
+    'start_date':datetime(2024,3,17,21,00)
+}
+
+with DAG('user_automation',
+    default_args=default_args,
+    schedule_interval='@daily',
+    catchup=False
+    ) as dag:
+    streaming_task = PythonOperator(
+        task_id = 'stream_data_from_api',
+        python_callable=stream_data
+    )
 
 
